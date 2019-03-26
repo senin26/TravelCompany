cdb9dd0f9501565e860b867058a75608bbb0b749
senin26 <sergey.ionin.7@gmail.com>
2019-03-26 21:58:49 +0300

    improved version

--- src/Application.java
+++ src/Application.java
@@ -1,6 +1,6 @@
 import common.solutions.utils.FileUtils;
 import country.domain.Country;
-import storage.initor.datasourcereader.CountryCityXmlStaxParser;
+import storage.initor.datasourcereader.CountryCityReader;
 
 import java.io.File;
 import java.io.IOException;
@@ -11,62 +11,27 @@ import java.util.List;
 
 public class Application {
 
-    public static class Reader extends Thread {
-        File fileWithXml;
-        List<Country> countries;
-        boolean done = false;
-        Object sync;
-
-        public Reader(String name, File fileWithXml, List<Country> countries, Object sync) {
-            this.fileWithXml = fileWithXml;
-            this.setName(name);
-            this.countries = countries;
-            this.sync = sync;
-        }
-
-        @Override
-        public void run() {
-            try {
-                countries = new CountryCityXmlStaxParser().parseFile(fileWithXml.getAbsolutePath());
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-            print(sync);
-            done = true;
-        }
-
-        private void print(Object sync) {
-            synchronized (sync) {
-                System.out.println(Thread.currentThread().getName());
-                for (Country country : countries) {
-                    System.out.println(country + "\n");
-                }
-            }
-        }
-
-    }
-
     public static void main(String[] args) {
+        CountryCityReader reader1 = null;
+        CountryCityReader reader2 = null;
         List<Country> countries1 = new ArrayList<>();
         List<Country> countries2 = new ArrayList<>();
         File fileWithXml1 = null;
         File fileWithXml2 = null;
-        Reader reader1 = null;
-        Reader reader2 = null;
         Object sync = new Object();
         try {
             fileWithXml1 = FileUtils.createFileFromResource("init_data1", ".xml", "/init_data1.xml");
             fileWithXml2 = FileUtils.createFileFromResource("init_data2", ".xml", "/init_data2.xml");
-            reader1 = new Reader("Reader: Russia, USA", fileWithXml1, countries1, sync);
+            reader1 = new CountryCityReader("Reader: Russia, USA", fileWithXml1, countries1, sync);
             reader1.start();
-            reader2 = new Reader("Reader: Belgium, Netherlands", fileWithXml2, countries2, sync);
+            reader2 = new CountryCityReader("Reader: Belgium, Netherlands", fileWithXml2, countries2, sync);
             reader2.start();
             /*reader1.join();
             reader2.join();*/
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
-            if (fileWithXml1 != null && fileWithXml2 != null && reader1.done && reader2.done) {
+            if (fileWithXml1 != null && fileWithXml2 != null && reader1.isDone() && reader2.isDone()) {
                 try {
                     Files.delete(Paths.get(fileWithXml1.toURI()));
                     Files.delete(Paths.get(fileWithXml2.toURI()));
